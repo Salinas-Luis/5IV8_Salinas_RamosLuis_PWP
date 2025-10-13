b3cf81abcc28d3d4b7fdec9844fffdd428ba6281
Salinas-Luis <lsalinasr2300@alumno.ipn.mx>
2025-10-12 19:26:17 -0600

    CSS, Modifique Estructura HTML, Corregi Ejercicio 3

--- 06Algoritmia/js/algoritmia.js
+++ 06Algoritmia/js/algoritmia.js
@@ -2,7 +2,6 @@ function validarl(e){
     var teclado = (document.all)? e.keyCode : e.which;
     if(teclado == 8) return true;
     var patron = /[a-zA-z\s ]/;
-
     var codigo = String.fromCharCode(teclado);
     return patron.test(codigo);
 }
@@ -34,7 +33,7 @@ function validarl2(e){
     return patron.test(codigo);
 }
 function calcularcu(){
-    var abcOk = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","V","X","Y","Z","ñ"];
+    var abcOk = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","V","X","Y","Z","Ñ"];
     var palabrasingresadas= document.getElementById("p3_input").value;
     var arreglopalabras = palabrasingresadas.split(",");
     var letra1 = "";
@@ -55,13 +54,16 @@ function calcularcu(){
                 if(letra1===letra2){
                     abcOk.splice(i,1);
                     contador= contador+1;
+                    i--;
+                    break;
                 }
                 }   
             }
-            var abcOk = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","V","X","Y","Z","ñ"];
+            var abcOk = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","V","X","Y","Z","Ñ"];
             arreglocontadorcaracteresunicos.push(contador); 
             contador=0;
         }
+        alert(arreglocontadorcaracteresunicos)
         contador = Math.max(...arreglocontadorcaracteresunicos);
         contador = arreglocontadorcaracteresunicos.indexOf(contador);
         document.getElementById("p3_output").innerHTML = "La palabra que tiene mas caracteres unicos es: " + arreglopalabras[contador];
