510ae3cf3e27f118d2afbc2c70868e3e07fa942e
Salinas-Luis <lsalinasr2300@alumno.ipn.mx>
2025-10-18 10:16:25 -0600

    Validacion campos vacios

--- 07 JSIntermedio/js/funcionesintermedio.js
+++ 07 JSIntermedio/js/funcionesintermedio.js
@@ -103,21 +103,29 @@ function problema1(){
     var num1 = parseFloat(document.getElementById("n1").value);
     var num2 = parseFloat(document.getElementById("n2").value);
     var resultado=0;
+    if(!isNaN(num1) && !isNaN(num2)){
     if(num1===num2){
       resultado = num1*num2  
     }else{
     num1>num2 ? resultado = num1-num2 : resultado = num1+num2
     }
     document.getElementById("output_p1").textContent = "El resultado es: " + resultado.toFixed(2)
+    }else{
+        alert("Por favor rellene los campos")
+    }
 }
 
 function problema2(){
     var num1 = parseFloat(document.getElementById("n1_p2").value);
     var num2 = parseFloat(document.getElementById("n2_p2").value);
     var num3 = parseFloat(document.getElementById("n3_p2").value);
+    if(!isNaN(num3) && !isNaN(num3) && !isNaN(num3)){
     var arreglonums = [num1,num2,num3];
     var resultado = Math.max(...arreglonums)
     document.getElementById("output_p2").textContent = "El numero mayor es: " + resultado.toFixed(2);
+    }else{
+        alert("Por favor rellene los campos")
+    }
 }
 
 function validarne(e){
@@ -132,6 +140,7 @@ function problema3(){
     var sueldobase = parseFloat(document.getElementById("sueldob").value)
     var horas = parseFloat(document.getElementById("horasb").value)
     var total=0;
+    if(!isNaN(sueldobase) && !isNaN(horas)){
     if(horas-40>0){
         total= sueldobase*40
         if(horas-48>0){
@@ -145,11 +154,15 @@ function problema3(){
         total = horas*sueldobase;
     }
     document.getElementById("output_p3").textContent = "Su sueldo final es: " + total.toFixed(2)
+    }else{
+     alert("Por favor rellene los campos")   
+    }
 }
 
 function problema4(){
     var nummeses = parseFloat(document.getElementById("nmeses").value)
     var sueldobase = parseFloat(document.getElementById("sueldobp4").value)
+    if(!isNaN(nummeses) && !isNaN(sueldobase)){
     var tiempoempresa = nummeses/12;
     var utilidadanual =0;
     if(tiempoempresa<1){
@@ -168,4 +181,7 @@ function problema4(){
         utilidadanual = sueldobase*0.2;
     }
     document.getElementById("output_p4").textContent = "Su reparto de utilidad anual es: " + utilidadanual.toFixed(2)
+    }else{
+     alert("Por favor rellene los campos")   
+    }
 }
